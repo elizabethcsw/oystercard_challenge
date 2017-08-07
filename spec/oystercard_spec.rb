2ea9740b0c3fcb8e3ddb9d960350852ba71cdb20
Elizabeth Chan <elizabeth.schan@outlook.com>
2017-08-07 19:34:49 +0100

    step 5

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,11 @@ describe Oystercard do
   it 'initializes with a zero balance' do
     expect(described_class.new.balance).to eq 0
   end
+  it 'can top up the balance' do
+    subject.top_up(20)
+    expect(subject.balance).to eq 20
+  end
+  it 'can top up the balance' do
+    expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+  end
 end
