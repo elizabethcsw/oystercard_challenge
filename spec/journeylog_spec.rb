e040f06fe5e4dd40052264c177418ae9dba70321
stephengeller <stephen.geller@hotmail.com>
2017-08-11 13:31:22 +0100

    Added tests for journeylog to spec

--- spec/journeylog_spec.rb
+++ spec/journeylog_spec.rb
@@ -4,6 +4,7 @@ require 'journeylog'
 describe JourneyLog do
 
   let (:station1) {double :station1}
+  let (:station2) {double :station2}
   let (:journey) {double :journey}
   let (:journey_c) {double :journey_c}
   subject {described_class.new(journey_c)}
@@ -22,12 +23,43 @@ describe JourneyLog do
   end
 
   context '#current_journey' do
-    it 'knows my current incomplete journey' do
+    before do
       subject.start(station1)
+    end
+    it 'knows my current incomplete journey' do
       allow(journey).to receive(:complete?).and_return(false)
       expect(subject.current_journey).to eq journey
+    end
   end
 
 
-end
+  context '#finish' do
+    before do
+      allow(journey).to receive(:out=).with(station2).and_return(station2)
+      allow(journey).to receive(:out).and_return(station2)
+    end
+    it 'applies an exit station to the most recent journey' do
+      allow(journey).to receive(:complete?).and_return(false)
+      subject.start(station1)
+      subject.finish(station2)
+      expect(subject.journeys.last.out).to eq station2
+    end
+  end
+
+  context '#journeys' do
+
+    it 'is empty when initialized' do
+      expect(subject.journeys).to be_empty
+    end
+
+    it 'shows a logged journey' do
+      allow(journey).to receive(:out=).with(station2).and_return(station2)
+      allow(journey).to receive(:out).and_return(station2)
+      allow(journey).to receive(:complete?).and_return(false)
+      subject.start(station1)
+      subject.finish(station2)
+      expect(subject.journeys).to include(journey)
+    end
+
+  end
 end
